baf261e659297f8111b3e135f4c537f64861dc25
aa <szerr@sina.cn>
2017-10-10 08:51:46 +0800

    关闭文件

--- header/datelog.go
+++ header/datelog.go
@@ -1,27 +1,28 @@
 package header
 
 import (
+	"io"
 	"os"
 	"time"
-	"io"
 )
 
-func DateLog(path string) io.Writer{
+func DateLog(path string) io.Writer {
 	var writer io.Writer = &dateLog{Path: path}
 	return writer
 }
 
-type dateLog struct{
-	day int
-	fd  *os.File
+type dateLog struct {
+	day  int
+	fd   *os.File
 	Path string
 }
 
-func (d *dateLog)Write(p []byte)(n int, err error){
+func (d *dateLog) Write(p []byte) (n int, err error) {
 	nowDay := time.Now().Day()
-	if d.day != nowDay{
+	if d.day != nowDay {
 		d.day = nowDay
-		d.fd, err = os.OpenFile(d.Path + time.Now().Format("2006-01-02") + ".log", os.O_APPEND|os.O_CREATE, 0644)
+		d.fd.Close()
+		d.fd, err = os.OpenFile(d.Path+time.Now().Format("2006-01-02")+".log", os.O_APPEND|os.O_CREATE, 0644)
 		if err != nil {
 			return 0, err
 		}
